16704dd5eb887b1a9e55133f434f3b7bffa01e69
varsha <varshavsl.30@gmail.com>
2025-03-20 20:41:45 +0530

    updates

--- doc_generator.py
+++
@@ -1,8 +0,0 @@
-from docxtpl import DocxTemplate
-
-doc = DocxTemplate("invoice_template.docx")
-
-invoice_list = [[2,"pen", 10, 20]]
-
-doc.render({"name": "john", "invoice_list": invoice_list})
-doc.save("new_invoice.docx")
